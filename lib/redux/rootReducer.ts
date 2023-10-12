ec90050858a73f1f1d0109de509e532f123c9676
Alphar <swamhtet129@gmail.com>
2023-10-13 02:50:42 +0630

    3rd deploy fixed

--- lib/redux/rootReducer.ts
+++ lib/redux/rootReducer.ts
@@ -1,10 +1,9 @@
 /* Instruments */
-import {counterSlice, todoSlice} from './slices'
+import {counterSlice} from './slices'
 import {movieSlice} from "@/lib/redux/slices/movieSlice";
 
 export const reducer = {
   counter: counterSlice.reducer,
-  todo: todoSlice.reducer,
   movie: movieSlice.reducer,
 
 
